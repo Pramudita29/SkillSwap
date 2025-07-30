8cafde6226bb05dff6816f41caf255c844e16120
Pramudita <pbhattarai0129@gmail.com>
2025-07-30 18:13:48 +0545

    configure Vite server for HTTPS with CORS support and local development settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'vite';
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve('D:/skillswap-backend/cert', 'server.key')),
+      cert: fs.readFileSync(path.resolve('D:/skillswap-backend/cert', 'server.crt')),
+    },
+    port: 3000, // Port for local development
+    cors: {
+      origin: 'https://localhost:3000', // Ensure this matches the frontend's URL
+      credentials: true,
+    },
+  },
+});
